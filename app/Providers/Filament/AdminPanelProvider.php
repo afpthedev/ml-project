09653fdc2b613248bc83996275ab9d094b4f4ca6
Ahmet Faruk Pala <65506131+afpthedev@users.noreply.github.com>
2024-10-15 22:08:08 +0300

    login güncelleme

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -31,10 +31,6 @@ class AdminPanelProvider extends PanelProvider
             ->default()
             ->id('admin')
             ->path('admin')
-            ->login()
-            ->registration()
-        ->passwordReset()
-        ->emailVerification()
         ->profile(Pages\Auth\EditProfile::class)
             ->colors([
                 'primary' => Color::Amber,
