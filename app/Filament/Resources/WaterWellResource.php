2c1278b17ffe9d75b6ea6dd2d6d21a86f1a2b84a
Ahmet Faruk Pala <65506131+afpthedev@users.noreply.github.com>
2024-10-22 20:26:20 +0300

    Güncellemeler

--- app/Filament/Resources/WaterWellResource.php
+++ app/Filament/Resources/WaterWellResource.php
@@ -20,6 +20,8 @@ class WaterWellResource extends Resource
 {
     protected static ?string $model = WaterWell::class;
 
+    protected static ?string $navigationGroup = 'Projeler';
+
     protected static ?string $navigationIcon = 'heroicon-o-globe-europe-africa';
     protected static ?string $navigationLabel = 'Su Kuyusu';
     protected static ?string $pluralLabel = 'Su Kuyuları';
