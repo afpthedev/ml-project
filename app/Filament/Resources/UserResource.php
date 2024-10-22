a2b449e803eecbce98e28db401e7956fa6d0003f
Ahmet Faruk Pala <65506131+afpthedev@users.noreply.github.com>
2024-10-22 20:38:12 +0300

    Güncellemeler

--- app/Filament/Resources/UserResource.php
+++ app/Filament/Resources/UserResource.php
@@ -17,6 +17,7 @@ class UserResource extends Resource
 {
     protected static ?string $model = User::class;
 
+    protected static ?string $navigationGroup = 'Kullanıcı İşlemleri';
 
 
     protected static ?string $navigationIcon = 'heroicon-o-users';
